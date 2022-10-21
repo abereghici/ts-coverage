53472057236539857e8943528589a30a1d062491
Alexandru Bereghici <abereghici@tripadvisor.com>
2022-10-21 17:26:06 +0300

    find js,ts files and display the percentage of coverage

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,40 @@
 use std::collections::HashMap;
-use std::{env, process};
+use std::ffi::OsStr;
+use std::fs::{self, DirEntry};
+use std::path::Path;
+use std::{env, io, process};
 
-fn is_js(ext: &str) -> bool {
-    ["js", "jsx", "cjs", "mjs", "cjsx", "mjsx"].contains(&ext)
+struct Extension<'a> {
+    name: &'a str,
+    variations: Vec<&'a str>,
 }
 
-fn is_ts(ext: &str) -> bool {
-    ["ts", "tsx", "cts", "mts", "ctsx", "mtsx"].contains(&ext)
+impl<'a> Extension<'a> {
+    pub fn new(name: &'a str, mut variations: Vec<&'a str>) -> Self {
+        if !variations.contains(&name) {
+            variations.push(name.clone())
+        }
+        Extension { name, variations }
+    }
+
+    pub fn r#match(&self, extension: &str) -> bool {
+        self.variations.contains(&extension)
+    }
+}
+
+fn visit_dirs(dir: &Path, cb: &mut dyn FnMut(&DirEntry)) -> io::Result<()> {
+    if dir.is_dir() {
+        for entry in fs::read_dir(dir)? {
+            let entry = entry?;
+            let path = entry.path();
+            if path.is_dir() {
+                visit_dirs(&path, cb)?;
+            } else {
+                cb(&entry);
+            }
+        }
+    }
+    Ok(())
 }
 
 fn main() {
@@ -18,45 +46,41 @@ fn main() {
     }
 
     let path = &args[1];
+    let mut extensions_hash: HashMap<&str, i32> = HashMap::new();
+    let mut files_count = 0;
 
-    let files = vec![
-        "index.js",
-        "index.ts",
-        "index.jsx",
-        "index.tsx",
-        "index.cjs",
-        "index.cts",
-        "index.mjs",
-        "index.mts",
-        "index.cjsx",
-        "index.ctsx",
-        "index.mjsx",
-        "index.mtsx",
+    let extensions = [
+        Extension::new("js", vec!["jsx", "cjs", "mjs", "cjsx", "mjsx"]),
+        Extension::new("ts", vec!["ts", "tsx", "cts", "mts", "ctsx", "mtsx"]),
     ];
 
-    let mut extensions_hash: HashMap<&str, i32> = HashMap::new();
+    let result = visit_dirs(&Path::new(path), &mut |file| {
+        let path = file.path();
+        let current_extension = path.extension().and_then(OsStr::to_str);
 
-    for file in files {
-        let ext = file.split(".").last();
-
-        if let Some(ext) = ext {
-            let mut increase_count = |parent: &'static str| {
-                *extensions_hash.entry(parent).or_insert(0) += 1;
-            };
-
-            if is_js(ext) {
-                increase_count("js")
-            }
-
-            if is_ts(ext) {
-                increase_count("ts")
+        if let Some(current_extension) = current_extension {
+            for ext in &extensions {
+                if ext.r#match(current_extension) {
+                    *extensions_hash.entry(ext.name).or_insert(0) += 1;
+                    files_count += 1;
+                    break;
+                }
             }
         }
+    });
+
+    if result.is_err() {
+        eprintln!("Error occurred: {}", result.unwrap_err());
+        process::exit(1);
     }
 
     for (extension, number) in extensions_hash {
-        println!("{}, {}", extension, number);
+        let bar_size = 60.0;
+        let percentage = (number as f32 * 100.0) / files_count as f32;
+        let percentage_bar = (number as f32 * bar_size) / files_count as f32;
+        print!(".{}(x)", extension);
+        print!(" | {:#<1$}", "", percentage_bar as usize);
+        print!("{:>1$} | ", "", (bar_size - percentage_bar) as usize);
+        println!("{} ({:.1}%)", number, percentage);
     }
-
-    dbg!(path);
 }
